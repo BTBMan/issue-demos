c3a066e6ac4f86c6593789b9ca1b9cb30516adcf
BTBMan <1498118157@qq.com>
2021-12-31 17:05:19 +0800

    增加依赖复现问题

---
+++ src/module.d.ts
@@ -0,0 +1,1 @@
+declare module 'vue-easy-lightbox/dist/vue-easy-lightbox.es5.common.min.js';
---
+++ src/vue.d.ts
@@ -0,0 +1,8 @@
+declare module 'vue' {
+  interface ComponentCustomProps {
+    //
+    onAbc?: () => void;
+  }
+}
+
+export {};

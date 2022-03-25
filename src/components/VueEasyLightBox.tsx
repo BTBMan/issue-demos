9fc5113ee0b73abd2cf827dce198c5d1080d23c6
BTBMan <1498118157@qq.com>
2022-03-25 19:29:41 +0800

    just test for git commit

--- src/components/VueEasyLightBox.tsx
+++ src/components/VueEasyLightBox.tsx
@@ -1,6 +1,7 @@
 import { defineComponent } from 'vue';
 import VueEasyLightbox from 'vue-easy-lightbox'; // 这样引入会有问题 请看App.tsx文件
 // import VueEasyLightbox from 'vue-easy-lightbox/dist/vue-easy-lightbox.es5.common.min.js'; // 这么引入就没有问题 并在module.d.ts里指定的类型是any
+// just test for git commit
 
 export default defineComponent({
   emits: ['close'],

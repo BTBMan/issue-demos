d941c11d3801bff915829857f467a2f09c70c2ea
BTBMan <1498118157@qq.com>
2022-08-02 16:59:33 +0800

    chore: nothing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { createApp } from 'vue';
+// import 'element-plus/dist/index.css';
 import App from './App';
 import { MyDirective } from './my-directive';
+
+// import('element-plus');
+
+// @ts-ignore
+if (window.a) {
+  import('element-plus/dist/index.css');
+}
 
 const app = createApp(App);
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vue from '@vitejs/plugin-vue';
+import qiankun from 'vite-plugin-qiankun';
 import AutoImport from 'unplugin-auto-import/vite';
+
+const { name } = require(`./package.json`);
 
 export default defineConfig({
   plugins: [
@@ -11,17 +14,22 @@ export default defineConfig({
       imports: ['vue'],
       dts: './src/auto-imports.d.ts',
     }),
+    qiankun(`${name}`, {
+      useDevMode: true,
+    }),
   ],
   server: {
     host: '0.0.0.0',
   },
   build: {
     rollupOptions: {
-      external: ['vue'],
+      // 'element-plus/dist/index.css'
+      external: ['vue', 'element-plus/dist/index.css'],
       output: {
         format: 'umd',
         globals: {
           vue: 'Vue',
+          'element-plus/dist/index.css': '""',
         },
       },
     },
